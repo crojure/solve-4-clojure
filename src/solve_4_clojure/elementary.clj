3516a61f44c73c4d37e4dbeb735fa9e5d222a09b
Todd Crone <tcrone@guaranteedrate.com>
2021-03-15 21:51:41 -0400

    Cap off elementary for now

--- src/solve_4_clojure/elementary.clj
+++ src/solve_4_clojure/elementary.clj
@@ -3,7 +3,7 @@
 ;; Regular Expressions
 (= "ABC" (apply str (re-seq #"[A-Z]+" "bA1B3Ce ")))
 
-;; Rearranging Code
+;; Rearranging Code ->
 (= (count (sort (rest (reverse [2 5 4 1 3 6]))))
    (-> [2 5 4 1 3 6]
        (reverse)
@@ -11,3 +11,20 @@
        (sort)
        (count))
    5)
+
+;; Recurring Theme
+(= [7 6 5 4 3]
+   (loop [x 5
+          result []]
+     (if (> x 0)
+       (recur (dec x) (conj result (+ 2 x)))
+       result)))
+
+;; Rearranging Code ->>
+(= (reduce + (map inc (take 3 (drop 2 [2 5 4 1 3 6]))))
+   (->> [2 5 4 1 3 6]
+        (drop 2)
+        (take 3)
+        (map inc)
+        (reduce +))
+   11)
