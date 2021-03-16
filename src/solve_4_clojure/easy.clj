f50246758ad2567e39c1b0b6c26b3e520613fef0
Todd Crone <tcrone@guaranteedrate.com>
2021-03-15 22:40:25 -0400

    Replicate a sequence

--- src/solve_4_clojure/easy.clj
+++ src/solve_4_clojure/easy.clj
@@ -10,3 +10,13 @@
       (recur (rest v1) (rest v2) (conj (conj result (first v1)) (first v2))))))
 
 (= (my-interleave [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c))
+
+;; Replicate a Sequence
+(defn replicate-seq [values n]
+  (reduce concat (map #(map (fn [x] %) (range 0 n)) values)))
+
+(replicate-seq [1 2 3] 2)
+
+(replicate-seq [[1 2] [3 4]] 2)
+
+(= (replicate-seq [1 2 3] 2) '(1 1 2 2 3 3))
